1791745c5a89df9f4707e5f03481305e8273f041
Paul Nechifor <paul@nechifor.net>
2014-12-20 14:49:41 +0000

    Add card tags.

--- site/cards/admin.py
+++ site/cards/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-from .models import Card, CardContent, CardAnswer
+from .models import Card, CardContent, CardAnswer, CardTag
 
 
 class CardContentInline(admin.StackedInline):
     model = CardContent
 
 
+class CardTagInline(admin.StackedInline):
+    model = CardTag
+    extra = 3
+
+
 class CardAdmin(admin.ModelAdmin):
-    inlines = [CardContentInline]
+    inlines = [CardContentInline, CardTagInline]
 
 
 admin.site.register(Card, CardAdmin)
--- site/cards/models.py
+++ site/cards/models.py
@@ -5,6 +5,11 @@ class Card(models.Model):
     pass
 
 
+class CardTag(models.Model):
+    card = models.ForeignKey(Card)
+    name = models.CharField(max_length=100)
+
+
 class CardContent(models.Model):
     card = models.OneToOneField(Card, related_name='content')
     front = models.TextField()
