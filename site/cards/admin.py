5a45bf22e35e6667552601653ea5696ac066d36d
Paul Nechifor <paul@nechifor.net>
2014-12-20 12:27:44 +0000

    Add CardAdmin.

--- site/cards/admin.py
+++ site/cards/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+from .models import Card, CardContent, CardAnswer
+
+
+class CardContentInline(admin.StackedInline):
+    model = CardContent
+
+
+class CardAdmin(admin.ModelAdmin):
+    inlines = [CardContentInline]
+
+
+admin.site.register(Card, CardAdmin)
