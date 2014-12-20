c4f93c3b8e73b40eed869b2b85dc74e991984761
Paul Nechifor <paul@nechifor.net>
2014-12-20 11:56:02 +0000

    Add card models.

--- site/cards/models.py
+++ site/cards/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+
+
+class CardContent(models.Model):
+    front = models.TextField()
+    back = models.TextField()
+
+
+class Card(models.Model):
+    content = models.ForeignKey(CardContent)
+
+
+class CardAnswer(models.Model):
+    card = models.ForeignKey(Card)
+    answer = models.TextField()
+    start_time = models.TimeField(auto_now=True)
+    end_time = models.TimeField()
+    score = models.FloatField()
