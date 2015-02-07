1935c1731aeef70779148c14c35a0aa3a650ce19
Paul Nechifor <paul@nechifor.net>
2015-02-07 11:01:24 +0000

    Redirect bare url.

--- site/cards/admin.py
+++ site/cards/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Card, CardContent, CardAnswer, CardTag
+from .models import Card, CardContent, CardTag
 
 
 class CardContentInline(admin.StackedInline):
--- site/project/urls.py
+++ site/project/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
+    url(r'^$', RedirectView.as_view(pattern_name='cards:check')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^cards/', include('cards.urls', namespace='cards')),
 )
